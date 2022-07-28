938d901e9c7b7a1ebb15cc6bde9454bc30f38d6e
Yashsankpal <53860409+Yashsankpal@users.noreply.github.com>
2022-07-28 10:24:28 +0530

    working

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = ()=>{
       <NavigationBar />
       <Layout>
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" exact element={<Home/>} />
           <Route path="/about"  element={<About/>} />
           <Route path="/contact"  element={<Contact/>} />
           <Route path="/login"  element={<Login/>}/>
